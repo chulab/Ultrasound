00681e0602d3f72294eac6fe708649c51ce067ad
Noahyt <NoahToyonaga@gmail.com>
2018-09-04 14:03:26 -0700

    add utility for converting matlab arrays to numpy arrays.

---
+++ matlab_python_utils/convert_mat_to_npy.py
@@ -0,0 +1,35 @@
+import numpy as np
+import scipy.io as sio
+import os
+import matplotlib.pyplot as plt
+import scipy.ndimage as ndimage
+
+ang = np.arange(1,10)
+data_location = "../data/5_17_manual_wrist/"
+file_name = os.path.join(data_location, "noah_palm_step_set1.mat")
+temp_data = sio.loadmat(file_name)
+raw_data = temp_data['img_int']
+
+raw_data = np.swapaxes(raw_data,2,0)
+raw_data = np.swapaxes(raw_data,1,2)
+
+print(raw_data.shape)
+
+sample_image_file = os.path.join(data_location, "example")
+for i in range(raw_data.shape[0]):
+    plt.imsave(sample_image_file+str(i), raw_data[i])
+
+print(raw_data[0])
+
+# ### need to rescale to 0-255
+# max = np.amax(raw_data)
+# scaled_data = (raw_data*255/max).astype(np.uint8)
+#
+# print(scaled_data.shape)
+#
+#
+# print(scaled_data.shape)
+# # zoom_ratio = (512 / 1456) * (28 / 23)
+# # im_zoomed = ndimage.zoom(scaled_data, zoom=(1, zoom_ratio, 1), order=1)
+save_file = os.path.join(data_location,"raw_images")
+np.save(save_file, raw_data)
